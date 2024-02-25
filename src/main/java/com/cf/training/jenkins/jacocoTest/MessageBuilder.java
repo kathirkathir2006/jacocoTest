72a2e27c771e506eaac6feb63fd70f312e8db742
kathir-clayfin <kathiresan@clayfin.com>
2024-02-26 00:28:47 +0530

    added

---
+++ src/main/java/com/cf/training/jenkins/jacocoTest/MessageBuilder.java
@@ -0,0 +1,25 @@
+package com.cf.training.jenkins.jacocoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class MessageBuilder {
+
+	public String getMessage(String name) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (name == null || name.trim().length() == 0) {
+
+            result.append("Please provide a name!");
+
+        } else {
+
+            result.append("Hello " + name);
+
+        }
+        return result.toString();
+    }
+
+}
--- src/test/java/com/cf/training/jenkins/jacocoTest/AppTest.java
+++ src/test/java/com/cf/training/jenkins/jacocoTest/AppTest.java
@@ -1,25 +1,15 @@
 package com.cf.training.jenkins.jacocoTest;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-	@Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(3, 5);
-        assertEquals(8, result); // 3 + 5 = 8
-    }
-	@Test
-    public void testAdd1() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(3, 5);
-        assertEquals(5, result); // 3 + 5 = 8
-    }
+public class AppTest {
+	  @Test
+	    public void testNameMkyong() {
+
+	        MessageBuilder obj = new MessageBuilder();
+	        assertEquals("Hello mkyong", obj.getMessage("mkyong"));
+
+	    }
 }
